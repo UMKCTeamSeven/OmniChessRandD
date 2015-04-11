56d40a4daa1b505ab6666d25f3c6a3e4440d8478
VhatAmI <heavenyale@gmail.com>
2015-04-11 16:30:59 -0500

    First exception / Mover class
    Mover uses invalid to check for move validity, then  returns true if ok
    to move piece. Another class will update the board.

---
+++ ocFIX/app/src/main/java/com/donthaveawebsite/mhy/ocfix/Mover.java
@@ -0,0 +1,31 @@
+package com.donthaveawebsite.mhy.ocfix;
+
+/**
+ * Created by Matthew on 4/11/2015.
+ */
+public class Mover extends IsValid {
+    public boolean TryMove(Piece piece, Spot Source, Spot Destination) throws NullPointerException
+    {
+        if (piece == null || Destination == null || Source == null)
+        {
+            NullPointerException objectmissing = new NullPointerException("A piece or coordinate(s) are missing");
+            throw objectmissing;
+        }
+        try
+        {
+            //if related piece call is move valid with Sorce.r and Destination.r here (To be imeplamented)
+            return(IsMoveValid(piece, Source, Destination));
+        }
+        catch (NullPointerException objectmissing)  // if any of the parameters are null, something is very wrong
+        {  }
+
+        return false;
+    }
+
+
+}
+
+
+
+
+
