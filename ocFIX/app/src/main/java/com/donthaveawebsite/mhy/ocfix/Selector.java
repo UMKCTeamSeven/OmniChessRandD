908c1660f879c890fc333c17a6cabb065b277590
VhatAmI <heavenyale@gmail.com>
2015-04-30 00:27:31 -0500

    Player Class

--- ocFIX/app/src/main/java/com/donthaveawebsite/mhy/ocfix/Selector.java
+++ ocFIX/app/src/main/java/com/donthaveawebsite/mhy/ocfix/Selector.java
@@ -1,21 +1,35 @@
 package com.donthaveawebsite.mhy.ocfix;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class Selector
-{
-    public Selector()
-    {}
-
+{   private List<Player> players = new ArrayList();
+    private Updater theupdater;
+    public Selector(List<Player> players, List<Spot> toupdate )
+    {
+        this.players = players;
+        theupdater = new Updater(toupdate);
+    }
+    public String getCurrentPlayerColor()
+    {             //Todo change method so it is not dependant on the amount of players
+        if (players.get(0).IsTurn())
+        {
+            return players.get(0).getColor();
+        }
+        else
+            return players.get(1).getColor();
+    }
+    //updates control layer when pieces are selected and deselected
     public boolean Filter(Spot spotState)
     {
+        //Occupied and not available
         if (spotState.isOccupied() && !spotState.piece.isAvailable())
         {return false;}
-        if (spotState.isOccupied() && spotState.SpotState == 2)
-        {return true;}
-        else
-        {return false;}
+        //Occupied and is the piece of the player whose turn it is
+        return (spotState.isOccupied() && spotState.SpotState == 2 && spotState.getpiece().getcolor(spotState.getpiece()).equals(this.getCurrentPlayerColor()));
+
     }
 
     public boolean SelectedMode( Piece piece , Spot spotState, List<Spot> spots)
@@ -58,7 +72,7 @@ public class Selector
 
         }
 
-        
+
    }
 
 
