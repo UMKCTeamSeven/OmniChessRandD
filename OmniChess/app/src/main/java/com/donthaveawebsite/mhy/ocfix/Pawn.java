7d2c68dab87365cac125aeca72f2223995879cce
Charles Thompson <charles.thompson4748@gmail.com>
2016-05-06 23:14:17 -0500

    Promotion Function created
    function takes a piece object and integer for promotion type and creates
    a piece objects for the corresponding selection and returns the newly
    created object. I also added a elseif statement in the OnMove() function
    to check to see if the location is occupied and if the location is the
    edge on the board. This statement will be changed in the future when I
    have a better understanding of the program flow

--- OmniChess/app/src/main/java/com/donthaveawebsite/mhy/ocfix/Pawn.java
+++ OmniChess/app/src/main/java/com/donthaveawebsite/mhy/ocfix/Pawn.java
@@ -1,14 +1,14 @@
 package com.donthaveawebsite.mhy.ocfix;
 
+import java.util.PriorityQueue;
+
 /**
  * Created by Matthew on 3/29/2015.
  */
-public class Pawn extends Piece
-{
+public class Pawn extends Piece {
 
 
-    public Pawn (Boolean available, int x, int y, int z)
-    {
+    public Pawn(Boolean available, int x, int y, int z) {
         super(available, x, y, z);
         this.type = new IsValid(IsValid.piecetype.pawn);
     }
@@ -18,45 +18,48 @@ public class Pawn extends Piece
         return pawn.getMC() == 0;
     }
 
+    public Piece Promote(Piece pawn, int PromotionSelection) {
+        Piece promotedPawn = null;
+        if (PromotionSelection == 0) {
+            promotedPawn = new Bishop(true, pawn.getX(), pawn.getY(), pawn.getZ());
+        } else if (PromotionSelection == 1) {
+            promotedPawn = new Rook(true, pawn.getX(), pawn.getY(), pawn.getZ());
+        } else if (PromotionSelection == 2) {
+            promotedPawn = new Knight(true, pawn.getX(), pawn.getY(), pawn.getZ());
+        } else if (PromotionSelection == 3) {
+            promotedPawn = new Queen(true, pawn.getX(), pawn.getY(), pawn.getZ());
+        } else
+            promotedPawn = pawn;
+        return promotedPawn;
+    }
+
     @Override
-    public void OnMove(Piece pawn, Spot source, Spot destination, Board theboard)
-    {
-        if (source == destination)
-        {
+    public void OnMove(Piece pawn, Spot source, Spot destination, Board theboard) {
+        if (source == destination) {
             //exit selected mode
             return;
         }
 
         Mover theMove = new Mover();
         boolean moved = theMove.TryMove(pawn, source, destination, theboard);
-        try
-        {
-        if (moved)
-        {
+        try {
+            if (moved) {
 
-            pawn.setMC(pawn.getMC() + 1);
-            if (destination.isOccupied()) {
-                destination.releaseSpot();
-                destination.placePiece(pawn);
+                pawn.setMC(pawn.getMC() + 1);
+                if (destination.isOccupied()) {
+                    destination.releaseSpot();
+                    destination.placePiece(pawn);
+                } else if (!destination.isOccupied() && pawn.getY() == 7 || pawn.getY() == 0) { //need to check for y-axis edge of board
+                    destination.placePiece(Promote(pawn, 0));
+                } else {
+                    destination.placePiece(pawn);
+                }
+                //update view.
             } else {
-                destination.placePiece(pawn);
             }
-            //update view.
-        } else {   } }        //end if block      //end try block   //return to selected mode TODO fill else block with return to selected mode
-        catch (NullPointerException objectmissing)
-        {  }
+        }        //end if block      //end try block   //return to selected mode TODO fill else block with return to selected mode
+        catch (NullPointerException objectmissing) {
+        }
 
-      }
-
-        //else
-//             if (relatedpiece)
-//             {
-//            //logic to fetch related pieces source
-//            //return OnMove(pawn, source ,destination); //checks if the destination works for the related piece.
-//             }
-
-
-}//endclass
-
-
-
+    }
+}
