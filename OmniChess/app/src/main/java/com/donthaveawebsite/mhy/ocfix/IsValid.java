63b3100b47b57f58b78151a8058ff30d4b193249
VhatAmI <heavenyale@gmail.com>
2016-05-08 18:10:17 -0500

    Pieces Go through portals?
    Testing commit

--- OmniChess/app/src/main/java/com/donthaveawebsite/mhy/ocfix/IsValid.java
+++ OmniChess/app/src/main/java/com/donthaveawebsite/mhy/ocfix/IsValid.java
@@ -68,38 +68,103 @@ public class IsValid
 
     public Spot OneN(Spot Source, Board theboard)
     {
+        if (theboard.getSpot(Source.x, (Source.y + 1)).isOccupied())
+        {
+            if (theboard.getSpot(Source.x, (Source.y + 1)).getpiece().getcolor(theboard.getSpot(Source.x, (Source.y + 1)).getpiece()) == 'P') {
+                return OneN(theboard.getSpot(theboard.getSpot(Source.x, (Source.y + 1)).getpiece().GetRelated().getX(), theboard.getSpot(Source.x, (Source.y + 1)).getpiece().GetRelated().getY()), theboard);
+            }
+
+        }
+        //original logic before portals
         return (theboard.getSpot(Source.x, (Source.y + 1)));
     }
     public Spot OneS(Spot Source, Board theboard)
     {
+        if (theboard.getSpot(Source.x, (Source.y-1)).isOccupied())
+        {
+            if (theboard.getSpot(Source.x, (Source.y - 1)).getpiece().getcolor(theboard.getSpot(Source.x, (Source.y - 1)).getpiece()) == 'P') {
+                return OneS(theboard.getSpot(theboard.getSpot(Source.x, (Source.y - 1)).getpiece().GetRelated().getX(), theboard.getSpot(Source.x, (Source.y - 1)).getpiece().GetRelated().getY()), theboard);
+            }
+        }
+
+
+        //original logic before portals
         return (theboard.getSpot(Source.x, (Source.y - 1)));
     }
 
     public Spot OneR(Spot Source, Board theboard)
     {
+        if (theboard.getSpot(Source.x + 1, (Source.y)).isOccupied())
+        {
+            if (theboard.getSpot(Source.x + 1, (Source.y)).getpiece().getcolor(theboard.getSpot(Source.x + 1, (Source.y)).getpiece()) == 'P') {
+                return OneR(theboard.getSpot(theboard.getSpot(Source.x + 1, (Source.y)).getpiece().GetRelated().getX(), theboard.getSpot(Source.x + 1, (Source.y)).getpiece().GetRelated().getY()), theboard);
+            }
+        }
+        //original logic before portals
         return (theboard.getSpot(Source.x + 1, (Source.y)));
     }
     public Spot OneL(Spot Source, Board theboard)
     {
+        if (theboard.getSpot(Source.x-1, (Source.y)).isOccupied())
+        {
+            if (theboard.getSpot(Source.x - 1, (Source.y)).getpiece().getcolor(theboard.getSpot(Source.x - 1, (Source.y)).getpiece()) == 'P') {
+                return OneL(theboard.getSpot(theboard.getSpot(Source.x - 1, (Source.y)).getpiece().GetRelated().getX(), theboard.getSpot(Source.x - 1, (Source.y)).getpiece().GetRelated().getY()), theboard);
+            }
+        }
+
+        //original logic before portals
         return (theboard.getSpot(Source.x - 1, (Source.y)));
     }
 
 
     public Spot OneLN(Spot Source, Board theboard) throws ArrayIndexOutOfBoundsException
     {
+        if(theboard.getSpot(Source.x - 1, (Source.y + 1)).isOccupied())
+        {
+            if (theboard.getSpot(Source.x - 1, (Source.y + 1)).getpiece().getcolor(theboard.getSpot(Source.x - 1, (Source.y + 1)).getpiece()) == 'P')
+            {
+                return OneLN(theboard.getSpot(theboard.getSpot(Source.x - 1, (Source.y + 1)).getpiece().GetRelated().getX(), theboard.getSpot(Source.x - 1, (Source.y + 1)).getpiece().GetRelated().getY()), theboard);
+            }
+
+        }
+            //original logic before portals
             return (theboard.getSpot(Source.x - 1, (Source.y + 1)));
     }
-    public Spot OneLS(Spot Source, Board theboard) throws ArrayIndexOutOfBoundsException
-    {
+    public Spot OneLS(Spot Source, Board theboard) throws ArrayIndexOutOfBoundsException {
+
+        if(theboard.getSpot(Source.x - 1, (Source.y - 1)).isOccupied())
+        {
+            if (theboard.getSpot(Source.x - 1, (Source.y - 1)).getpiece().getcolor(theboard.getSpot(Source.x - 1, (Source.y - 1)).getpiece()) == 'P')
+            {
+                return OneLS(theboard.getSpot(theboard.getSpot(Source.x - 1, (Source.y - 1)).getpiece().GetRelated().getX(), theboard.getSpot(Source.x - 1, (Source.y - 1)).getpiece().GetRelated().getY()), theboard);
+            }
+
+        }
+            //original logic before portals
             return (theboard.getSpot(Source.x - 1, (Source.y - 1)));
     }
     public Spot OneRS(Spot Source, Board theboard) throws ArrayIndexOutOfBoundsException
     {
+        if (theboard.getSpot(Source.x + 1, (Source.y  -1)).isOccupied())
+        {
+            if (theboard.getSpot(Source.x + 1, (Source.y - 1)).getpiece().getcolor(theboard.getSpot(Source.x + 1, (Source.y - 1)).getpiece()) == 'P') {
+                return OneRS(theboard.getSpot(theboard.getSpot(Source.x + 1, (Source.y - 1)).getpiece().GetRelated().getX(), theboard.getSpot(Source.x + 1, (Source.y - 1)).getpiece().GetRelated().getY()), theboard);
+            }
+
+        }
+           //original logic before portals
             return (theboard.getSpot(Source.x + 1, (Source.y - 1)));
     }
 
     public Spot OneRN(Spot Source, Board theboard) throws ArrayIndexOutOfBoundsException
     {
+        if (theboard.getSpot(Source.x + 1, (Source.y +1)).isOccupied())
+        {
+            if (theboard.getSpot(Source.x + 1, (Source.y + 1)).getpiece().getcolor(theboard.getSpot(Source.x + 1, (Source.y + 1)).getpiece()) == 'P') {
+                return OneRN(theboard.getSpot(theboard.getSpot(Source.x + 1, (Source.y + 1)).getpiece().GetRelated().getX(), theboard.getSpot(Source.x + 1, (Source.y + 1)).getpiece().GetRelated().getY()), theboard);
+            }
+        }
+        //original logic before portals
         return (theboard.getSpot(Source.x + 1, (Source.y + 1)));
     }
 
@@ -112,6 +177,10 @@ public class IsValid
         return false;
     }
 
+    public boolean IsPortal(Spot spot)
+    {
+        return spot.getpiece().getcolor(spot.getpiece()) == 'P';
+    }
 
     private boolean portalLogicCheck(Piece piece,Spot Source,Spot Destination, Board theboard)
     {
