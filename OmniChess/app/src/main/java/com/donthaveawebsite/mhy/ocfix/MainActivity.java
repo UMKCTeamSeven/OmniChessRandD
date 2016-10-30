6e00182dca7c28a51717c1599a9b930735bf9134
VhatAmI <heavenyale@gmail.com>
2016-10-30 18:33:11 -0500

    Android Studio 2.2.2 Compatibility
    ActionBarActivity was depreciated

--- OmniChess/app/src/main/java/com/donthaveawebsite/mhy/ocfix/MainActivity.java
+++ OmniChess/app/src/main/java/com/donthaveawebsite/mhy/ocfix/MainActivity.java
@@ -1,15 +1,15 @@
 package com.donthaveawebsite.mhy.ocfix;
 
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.support.v7.app.ActionBarActivity;
-        import android.view.Menu;
-        import android.view.MenuItem;
-        import android.view.View;
-        import android.widget.TextView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
